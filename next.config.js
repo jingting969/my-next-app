3bf769c1af1186b837381e87aa1700c5e5291c60
1228227587@qq.com <1228227587@qq.com>
2025-01-24 17:38:06 +0800

    mjs、js、ts区别

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const { setupDevPlatform } = require("@cloudflare/next-on-pages/next-dev");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+if (process.env.NODE_ENV === "development") {
+  setupDevPlatform();
+}
+
+module.exports = nextConfig; 
