18b99884833c2eef92e8b71d6dd2068efe9410a3
1228227587@qq.com <1228227587@qq.com>
2025-01-24 17:03:06 +0800

    clear

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    webVitalsAttribution: ['CLS', 'LCP'],
+  }
+};
 
+// 使用 IIFE (立即调用函数表达式) 来处理异步操作
 if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
+  (async () => {
+    await setupDevPlatform();
+  })();
 }
 
 export default nextConfig;
