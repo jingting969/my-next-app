d99266cd85e4b0464956d1b101aabd0e650d8126
1228227587@qq.com <1228227587@qq.com>
2025-01-24 17:20:54 +0800

    clear

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -1,9 +1,9 @@
-import { createServerClient } from '../../lib/supabaseClient';
+import { getSupabaseClient } from "@/lib/db";
 
 export const runtime = "edge";
 
 export default async function Countries() {
-  const supabase = createServerClient();
+  const supabase = getSupabaseClient();
   const { data: countries } = await supabase.from("countries").select();
 
   return <pre>{JSON.stringify(countries, null, 2)}</pre>
