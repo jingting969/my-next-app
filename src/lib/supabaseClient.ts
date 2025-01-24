74a9e51aa63905c1cb9cfe09d1e5d9fc10c4c381
1228227587@qq.com <1228227587@qq.com>
2025-01-24 17:10:53 +0800

    clear

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
+export const runtime = "edge";
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
